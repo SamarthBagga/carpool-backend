9328a33ea6e48f094b43f077c9743d3a290242d7
devashish94 <roy.devashish94@gmail.com>
2024-02-12 17:36:16 +0530

    add temporary debug to server

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,14 +2,14 @@ const { verify } = require("jsonwebtoken");
 
 exports.authMiddleware = function (req, res, next) {
   const loginToken = req.cookies["secret-token"];
+  console.log(loginToken);
   if (!loginToken) {
     return res
       .status(401)
       .json({ success: false, message: "you are not authorized" });
   }
   try {
-    const decodedUser = verify(loginToken, process.env.SECRET_KEY);
-    req.user = decodedUser;
+    req.user = verify(loginToken, process.env.SECRET_KEY);
     next();
   } catch (err) {
     return res.json({
