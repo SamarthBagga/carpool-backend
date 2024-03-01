76cc3e8a844793e9edff6394b2b7a85a846d344d
devashish94 <roy.devashish94@gmail.com>
2024-03-02 01:36:30 +0530

    post ka get

--- src/routes/ride.route.js
+++ src/routes/ride.route.js
@@ -5,7 +5,7 @@ const { authMiddleware } = require("../middlewares/auth.middleware");
 const router = express.Router();
 
 router
-  .post("/get/:rideId", authMiddleware, rideController.byId)
+  .get("/get/:rideId", authMiddleware, rideController.byId)
   .post("/create", authMiddleware, rideController.createRide)
   .post("/request", authMiddleware, rideController.requestRide)
   .post("/search", authMiddleware, rideController.searchRides)
