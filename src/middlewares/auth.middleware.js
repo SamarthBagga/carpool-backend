83e4da0d5918c0877675c3313827100678e40a71
devashish94 <roy.devashish94@gmail.com>
2024-04-24 14:39:55 +0530

    changed stuff

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,28 +1,32 @@
 const { verify } = require("jsonwebtoken");
-const {User} = require("../models/user.model")
-exports.authMiddleware = async function  (req, res, next) {
+const User = require("../models/user.model");
+exports.authMiddleware = async function (req, res, next) {
   // const loginToken = req.cookies["secret-token"];
-  const authHeader = req.headers['Authorization'];
-  const loginToken = authHeader.split(" ")[1]
-
+  const authHeader = req.headers["authorization"];
+  const loginToken = authHeader.split(" ")[1];
   console.log(loginToken);
+
   if (!loginToken) {
     return res
       .status(401)
-      .json({ success: false, message: "Unauthorized, user not authenticated" });
+      .json({
+        success: false,
+        message: "Unauthorized, user not authenticated",
+      });
   }
   try {
-    const userFromToken = verify(loginToken, process.env.SECRET_KEY)
+    const userFromToken = verify(loginToken, process.env.SECRET_KEY);
     const dbUsr = await User.findById(userFromToken.id);
-    if(!dbUsr){
+    if (!dbUsr) {
       return res.status(401).json({
-        success:false,
-        message:"Valid token but user does not exists "
-      })
+        success: false,
+        message: "Valid token but user does not exists ",
+      });
     }
+    req.user = userFromToken;
     next();
   } catch (err) {
-    return res.json({
+    return res.status(401).json({
       success: false,
       message: "you are not authorized",
     });
