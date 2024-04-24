fe336e668a5784e51cfc5d0573df70b2b118450b
devashish94 <roy.devashish94@gmail.com>
2024-04-24 13:23:20 +0530

    changed login to token sent in response

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 const { verify } = require("jsonwebtoken");
-
-exports.authMiddleware = function (req, res, next) {
+const {User} = require("../models/user.model")
+exports.authMiddleware = async function  (req, res, next) {
   // const loginToken = req.cookies["secret-token"];
   const authHeader = req.headers['Authorization'];
   const loginToken = authHeader.split(" ")[1]
@@ -12,7 +12,14 @@ exports.authMiddleware = function (req, res, next) {
       .json({ success: false, message: "Unauthorized, user not authenticated" });
   }
   try {
-    req.user = verify(loginToken, process.env.SECRET_KEY);
+    const userFromToken = verify(loginToken, process.env.SECRET_KEY)
+    const dbUsr = await User.findById(userFromToken.id);
+    if(!dbUsr){
+      return res.status(401).json({
+        success:false,
+        message:"Valid token but user does not exists "
+      })
+    }
     next();
   } catch (err) {
     return res.json({
