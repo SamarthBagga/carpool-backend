32d973ed1e5f3e607028954fffe84158e183909a
devashish94 <roy.devashish94@gmail.com>
2024-05-10 00:36:03 +0530

    removed the available prod server url from docs

--- docs/servers.js
+++ docs/servers.js
@@ -1,8 +1,8 @@
 exports.servers = [
-  {
-    url: "https://carpool-backend-muj.onrender.com/api",
-    description: "Production Server on Render",
-  },
+  // {
+  //   url: "https://carpool-backend-muj.onrender.com/api",
+  //   description: "Production Server on Render",
+  // },
   {
     url: `http://localhost:4500/api`,
     description: "Development Server",
