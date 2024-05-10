429b4863abdd6e1b22c83e10dd0f0fee576513ba
devashish94 <roy.devashish94@gmail.com>
2024-05-10 16:34:29 +0530

    something

--- config-files/vps-init.sh
+++ config-files/vps-init.sh
@@ -8,12 +8,22 @@ echo "Updating the APT Repository..."
 apt update 
 apt install -y git nginx
 
+# change nvm location 
+if [ "$(id -u)" != "0" ]; then
+    export HOME=/home/$(logname)
+fi
+
 # Install NVM (Node Version Manager)
 echo "Installing NVM (Node Version Manager) ..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
 # Install Node.js and npm using NVM
 source $HOME/.bashrc
+
+if [ "$(id -u)" != "0" ]; then
+    export HOME=/home/$(logname)
+fi
+
 nvm install --lts
 
 # Clone your repository
@@ -29,7 +39,7 @@ npm install -g pm2
 cp config-files/nginx/carpool-backend.conf /etc/nginx/sites-available/carpool-backend.conf
 
 # Enable Nginx configuration
-sed -i '/http {/a \ \ limit_req_zone $binary_remote_addr zone=carpool_limit:10m rate=20r/s;' /etc/nginx/nginx.conf
+sed -i '/http {/a \ \ \ limit_req_zone $binary_remote_addr zone=carpool_limit:10m rate=20r/s;' /etc/nginx/nginx.conf
 ln -s /etc/nginx/sites-available/carpool-backend.conf /etc/nginx/sites-enabled/carpool-backend.conf 
 nginx -t && service nginx restart
 
