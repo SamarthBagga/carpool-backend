3a7fb37f0e889d942de37e227490ff081f22958f
devashish94 <roy.devashish94@gmail.com>
2024-05-10 22:38:44 +0530

    change localhost to 127.0.0.1 nginx

--- config-files/vps-init.sh
+++ config-files/vps-init.sh
@@ -9,20 +9,30 @@ apt update
 apt install -y git nginx
 
 # change nvm location 
-if [ "$(id -u)" != "0" ]; then
+if [ "$(logname)" != "root" ]; then
     export HOME=/home/$(logname)
 fi
+
+echo "[FIRST] current HOME location is $HOME"
 
 # Install NVM (Node Version Manager)
 echo "Installing NVM (Node Version Manager) ..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
 # Install Node.js and npm using NVM
-source $HOME/.bashrc
+# source $HOME/.bashrc
 
-if [ "$(id -u)" != "0" ]; then
+if [ "$(logname)" != "0" ]; then
     export HOME=/home/$(logname)
 fi
+
+echo "[SECOND] current HOME location is $HOME"
+
+chown -R $(logname):$(logname) .nvm .nvm/**/* 
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install --lts
 
@@ -35,6 +45,8 @@ echo "Installing Dependencies..."
 npm install
 npm install -g pm2
 
+chown -R $(logname):$(logname) . node_modules node_modules/**/* 
+
 # Copy Nginx configuration files
 cp config-files/nginx/carpool-backend.conf /etc/nginx/sites-available/carpool-backend.conf
 
@@ -43,8 +55,5 @@ sed -i '/http {/a \ \ \ limit_req_zone $binary_remote_addr zone=carpool_limit:10
 ln -s /etc/nginx/sites-available/carpool-backend.conf /etc/nginx/sites-enabled/carpool-backend.conf 
 nginx -t && service nginx restart
 
-echo "Starting Backend Server..."
-npm run prod-server
-
-echo "Deployment completed successfully!"
+chown -R $(logname):$(logname) $HOME/.nvm/**/*
 
