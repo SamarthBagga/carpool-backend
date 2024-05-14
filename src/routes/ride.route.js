25dd573cb0dfe21828738616d1239732b1c4cfc3
devashish94 <roy.devashish94@gmail.com>
2024-05-15 00:31:18 +0530

    duplicate request fix, cancel ride bug fix

--- src/routes/ride.route.js
+++ src/routes/ride.route.js
@@ -5,7 +5,7 @@ const { authMiddleware } = require("../middlewares/auth.middleware");
 const router = express.Router();
 
 router
-  .get("/get/:rideId", authMiddleware, rideController.byId)
+  .get("/get/:rideId", authMiddleware, rideController.getRideById)
   .post("/create", authMiddleware, rideController.createRide)
   .post("/request", authMiddleware, rideController.requestRide)
   .post("/search", authMiddleware, rideController.searchRides)
