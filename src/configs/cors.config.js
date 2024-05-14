3651745f55d4085650eaa613ef5f7f28517b057f
devashish94 <roy.devashish94@gmail.com>
2024-05-14 12:07:02 +0530

    cors *

--- src/configs/cors.config.js
+++ src/configs/cors.config.js
@@ -1,9 +1,5 @@
 exports.corsOptions = {
   /* add more urls to that array that need to be allowed */
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:5500",
-    "http://localhost:4173",
-  ],
+  origin: "*",
   credentials: true,
 };
